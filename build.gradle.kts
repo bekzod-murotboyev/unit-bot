322e988e09f8608d05d0ff2225766a4b378417d9
Bekzodbek Murotboyev <bekzodbek.muratboev@bellissimo.uz>
2025-10-03 09:52:00 +0500

    Reflect telegram bot dependency version upgraded to 1.5.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.3.0"
+    id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm")
 }
@@ -21,14 +21,14 @@ repositories {
     mavenCentral()
 }
 
-extra["springCloudVersion"] = "2023.0.3"
+extra["springCloudVersion"] = "2025.0.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
-    implementation("io.github.reflectframework:reflect-telegram-bot:1.5.2")
+    implementation("io.github.reflectframework:reflect-telegram-bot:1.5.5")
 
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.mysql:mysql-connector-j")
